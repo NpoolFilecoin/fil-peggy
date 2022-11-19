d61566487447bf26c97d063b2a43239b325f05d7
Zhao KK <kikakkz@hotmail.com>
2022-11-19 21:36:00 +0800

    Add cli framework

---
+++ cli/src/main.rs
@@ -0,0 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("{}", args.name);
+}
