a97364926eb0a89de93c554b0a9190ce24021d0e
Zhao KK <kikakkz@hotmail.com>
2022-11-19 22:51:11 +0800

    Add text menu

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use scanf::scanf;
 use std::str::FromStr;
+use colored::Colorize;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -27,9 +28,9 @@ impl FromStr for MenuItem {
 }
 
 fn select_menu() -> Result<MenuItem, String> {
-    println!("Action you want:");
-    println!("  1. Wallet");
-    println!("  2. Actor");
+    println!("{}", "Action you want:".green());
+    println!("{}{}", "  1".green(), ". Wallet".blue());
+    println!("{}{}", "  2".green(), ". Actor".blue());
 
     let mut action = MenuItem::Wallet;
     match scanf!("{}", action) {
@@ -40,11 +41,79 @@ fn select_menu() -> Result<MenuItem, String> {
     }
 }
 
+fn create_wallet() {
+    println!("{}", " Try create new wallet".yellow());
+}
+
+enum ActorMenuItem {
+    Compile,
+    Deploy,
+    Create,
+    TakeOwner,
+}
+
+impl FromStr for ActorMenuItem {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let item = s.parse::<i32>()?;
+        match item {
+            1 => Ok(Self::Compile),
+            2 => Ok(Self::Deploy),
+            3 => Ok(Self::Create),
+            4 => Ok(Self::TakeOwner),
+            _ => Ok(Self::Compile),
+        }
+    }
+}
+
+fn compile_actor() {
+    println!("{}", " Try compile actor".yellow());
+}
+
+fn deploy_actor() {
+    println!("{}", " Try deploy actor".yellow());
+}
+
+fn create_actor() {
+    println!("{}", " Try create actor".yellow());
+}
+
+fn take_owner() {
+    println!("{}", " Try take owner".yellow());
+}
+
+fn actor_handler() {
+    println!("{}", "Actor action you want:".green());
+    println!("{}{}", "  1".green(), ". Compile".blue());
+    println!("{}{}", "  2".green(), ". Deploy".blue());
+    println!("{}{}", "  3".green(), ". Create".blue());
+    println!("{}{}", "  4".green(), ". TakeOwner".blue());
+
+    let mut action = ActorMenuItem::Compile;
+    match scanf!("{}", action) {
+        Ok(_) => {
+            match action {
+                ActorMenuItem::Compile => compile_actor(),
+                ActorMenuItem::Deploy => deploy_actor(),
+                ActorMenuItem::Create => create_actor(),
+                ActorMenuItem::TakeOwner => take_owner(),
+            }
+        },
+        Err(err) => {
+            println!("{}", format!("  Fail to get actor menu input: {}", err).red());
+        },
+    }
+}
+
 fn main() {
     let _ = Args::parse();
 
     loop {
-        let menu = select_menu();
-        println!("{:?}", menu.unwrap())
+        let menu = select_menu().unwrap();
+        match menu {
+            MenuItem::Wallet => create_wallet(),
+            MenuItem::Actor => actor_handler(),
+        }
     }
 }
