c51b91d3f3fc5b419b75e9b74177ddaf7a167086
Zhao KK <kikakkz@hotmail.com>
2022-11-20 22:30:32 +0800

    Try to add state wait message

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -14,7 +14,10 @@ use fvm_shared::{
     message::Message,
     econ::TokenAmount,
 };
-use fil_actor_power::CreateMinerParams;
+use fil_actor_power::{
+    CreateMinerParams,
+    // CreateMinerReturn,
+};
 use libp2p::{
     identity::{ed25519, Keypair},
     PeerId,
@@ -28,13 +31,17 @@ use fil_actors_runtime::STORAGE_POWER_ACTOR_ADDR;
 use forest_message::signed_message::SignedMessage;
 use forest_json::{
     signed_message::json::SignedMessageJson,
+    cid::CidJson,
 };
 use reqwest::{
     blocking,
-    header::CONTENT_TYPE,
+    header::{CONTENT_TYPE, AUTHORIZATION},
 };
 use jsonrpc_v2::RequestObject;
-use forest_rpc_api::mpool_api;
+use forest_rpc_api::{
+    mpool_api,
+    state_api,
+};
 use serde_json::json;
 
 #[derive(Parser, Debug)]
@@ -191,10 +198,15 @@ fn create_miner() {
     let peer_id = PeerId::from(net_keypair.public());
     println!("{}{:?}", "    PeerId:".yellow(), peer_id);
 
-    let mut rpc_host = String::from("https://wallaby.node.glif.io/rpc/v0");
-    println!("{}{}", "Enter wallet rpc endpoint:".green(), format!(" (default https://wallaby.node.glif.io/rpc/v0)"));
+    let mut rpc_host = String::from("http://localhost:1234/rpc/v0");
+    println!("{}{}", "Enter wallet rpc endpoint:".green(), format!("(default {})", rpc_host));
     scanf!("{}", rpc_host).unwrap();
     println!("{}{}{}", "  You will use ".yellow(), rpc_host, " as your rpc host.".yellow());
+
+    let mut bearer_token = String::from("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiXX0.TrGlLJwD5kVqnlAKcQSoims5x4H904EWcqnrOFJWw5Y");
+    println!("{}{}", "Enter rpc bearer token: ".green(), format!("(default {})", bearer_token));
+    scanf!("{}", bearer_token).unwrap();
+    println!("{}{}{}", "You will use ".yellow(), bearer_token, " as your rpc access token.".yellow());
 
     let params = CreateMinerParams {
         owner,
@@ -210,11 +222,11 @@ fn create_miner() {
         from: owner,
         method_num: 2,
         value: TokenAmount::from_atto(1000),
-        sequence: 0,
+        sequence: 3,
         params: params,
-        gas_fee_cap: TokenAmount::from_nano(100000),
-        gas_limit: 0,
-        gas_premium: TokenAmount::from_atto(1000),
+        gas_fee_cap: TokenAmount::from_nano(10000),
+        gas_limit: 301000,
+        gas_premium: TokenAmount::from_atto(100),
     };
     let msg_cid = msg.cid().unwrap();
     let sig = forest_key_management::sign(
@@ -225,21 +237,53 @@ fn create_miner() {
     let smsg = SignedMessage::new_from_parts(msg, sig).unwrap();
     println!("{}", "  Create miner message:".green());
     println!("{}{:?}", "    CID:".yellow(), msg_cid);
+    println!("{}{:?}", "    CidJson:".yellow(), CidJson(msg_cid));
+    println!("{}{:?}", "    Signed CID:".yellow(), smsg.cid().unwrap());
+    println!("{}{:?}", "    Key type:".yellow(), key_info.key_type());
 
     let req = RequestObject::request()
-        .with_params(json!(SignedMessageJson(smsg.clone())))
+        .with_params(json!(vec![SignedMessageJson(smsg.clone())]))
         .with_method(mpool_api::MPOOL_PUSH)
         .with_id(7878)
         .finish();
 
-    let cli = blocking::Client::new();
+    let cli = blocking::Client::builder()
+        .timeout(std::time::Duration::from_secs(60))
+        .build()
+        .unwrap();
     let res = cli
-        .post(rpc_host)
+        .post(rpc_host.clone())
         .header(CONTENT_TYPE, "application/json")
+        .header(AUTHORIZATION, format!("Bearer {}", bearer_token))
         .json(&req)
         .send()
         .unwrap();
-    println!("{}{:?}", "Create miner: ".yellow(), res);
+    println!("{}", "Create miner: ".yellow());
+    println!("{}{}", "  Status: ".yellow(), res.status());
+    println!("{}{}", "  Message: ".yellow(), res.text().unwrap());
+    let req = serde_json::to_string(&req).unwrap();
+    println!("{}{:?}", "  Input: ".yellow(), req);
+
+    let params = json!([CidJson(smsg.cid().unwrap()), 900]);
+    let req = RequestObject::request()
+        .with_params(params.clone())
+        .with_method(state_api::STATE_WAIT_MSG)
+        .with_id(7879)
+        .finish();
+
+    println!("{}", "Wait create miner result: ".yellow());
+    println!("{}{:?}", "  Params: ".yellow(), params);
+
+    let res = cli
+        .post(rpc_host.clone())
+        .header(CONTENT_TYPE, "application/json")
+        .header(AUTHORIZATION, format!("Bearer {}", bearer_token))
+        .json(&req)
+        .send()
+        .unwrap();
+    println!("{}", "Create miner result: ".yellow());
+    println!("{}{}", "  Status: ".yellow(), res.status());
+    println!("{}{}", "  Message: ".yellow(), res.text().unwrap());
     let req = serde_json::to_string(&req).unwrap();
     println!("{}{:?}", "  Input: ".yellow(), req);
 }
