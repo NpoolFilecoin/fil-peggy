3349e45e5306e018d31eae558cde14f9573ec4ec
tangsong <tangsong0202@gmail.com>
2022-12-01 10:35:33 +0800

    fix action pre install proto compile

--- peggy/main.rs
+++ peggy/main.rs
@@ -1,18 +1,9 @@
-use anyhow::{anyhow, Error};
+use anyhow::Error;
 use app::App;
 use clap::Parser;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     logger::initialize();
-
-    let app = App::parse();
-    let cmd = match app.cmd.parse() {
-        Ok(cmd) => Ok(cmd),
-        Err(err) => Err(anyhow!("{}", err)),
-    };
-    match cmd?.run().await {
-        Ok(_) => Ok(()),
-        Err(err) => Err(anyhow!("{}", err)),
-    }
+    Ok(App::parse().cmd.parse()?.run().await?)
 }
