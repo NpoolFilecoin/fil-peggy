bcd60875f41ac2c45506e7c05dd9dff150cee8ce
Zhao KK <kikakkz@hotmail.com>
2023-01-29 16:18:00 +0800

    Add header implementation

--- extension/src/components/header.vue
+++ extension/src/components/header.vue
@@ -1,6 +1,12 @@
 <template>
   <div class='header'>
     <img :src='logoPath'/>
+    <div class='selector'>
+      <div class='indicator' />
+      <span class='network'>
+        {{ network }}
+      </span>
+    </div>
   </div>
 </template>
 
@@ -21,6 +27,31 @@ export default {
 .header {
   height: 64px;
   width: 100%;
-  background-color: #f2f4f6;
+  background-color: #F2F4F6;
+}
+
+.selector {
+  display: flex;
+  float: right;
+  border: 1px solid #B5B5B5;
+  border-radius: 20px;
+  height: 40px;
+  width: 176px;
+  margin: 12px 16px 12px 0px;
+  line-height: 40px;
+}
+
+.network {
+  font-size: 12px;
+  font-weight: bold;
+  color: #535A61;
+}
+
+.indicator {
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: #29B6AF;
+  margin: 10px;
 }
 </style>
